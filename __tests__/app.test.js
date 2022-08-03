35fa012b78523d5120268b31f31b7c7120987781
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-03 11:08:04 -0700

    wrote and passed first test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,13 +1,22 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
   
+  it('should return get a list of books', async () => {
+    const res = await request(app).get('/books');
+    expect (res.body.length).toEqual(11);
+    const gondor = res.body[0];
+    expect(gondor).toHaveProperty('title', 'Straight Outa Gondor');
+    expect(gondor).toHaveProperty('id');
+    expect(gondor).toHaveProperty('released');
+  });
+
   afterAll(() => {
     pool.end();
   });
