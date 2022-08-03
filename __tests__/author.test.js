c26241a39fba252b4e60b0f1d9e0a5b51e319315
Sebastian-Simek <sebastiansimek09@gmail.com>
2022-08-03 14:54:07 -0700

    added and passed post test for authors

---
+++ __tests__/author.test.js
@@ -0,0 +1,69 @@
+const pool = require('../lib/utils/pool');
+const setup = require('../data/setup');
+const request = require('supertest');
+const app = require('../lib/app');
+const e = require('express');
+
+describe('backend-express-template routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  
+
+  it('#GET should return a list of authors', async () => {
+    const res = await request(app).get('/authors');
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual([
+      { id: '1',
+        name: 'Brian Thomas',
+        dob: 1892,
+        pob: 'Toronto, Canada'
+      },
+      { id: '2',
+        name: 'Kat Zaro',
+        dob: 1744,
+        pob: 'Zagreb, Croatia'
+      },
+      { id: '3',
+        name: 'Amanda Hecht',
+        dob: 1910,
+        pob: 'Zurich, Switzerland'
+      },
+      { id: '4',
+        name: 'Amaya Alejandra',
+        dob: 1689,
+        pob: 'Tunis, Tunisia'
+      },
+      { id: '5',
+        name: 'Austin Han',
+        dob: 1933,
+        pob: 'Astoria, OR'
+      },
+      { id: '6',
+        name: 'David Francisco',
+        dob: 1922,
+        pob: 'New Orleans, LA'
+      }
+    ]);
+  });
+
+  it('#POST /should add a new author', async () => {
+    
+    const newAuthor = {
+      name: 'Mariah Schock',
+      dob: 1888,
+      pob: 'Bogota, Columbia'
+    };
+
+    const res = await (await request(app).post('/authors').send(newAuthor));
+    expect(res.status).toBe(200);
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      ...newAuthor,
+    });
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
+});
